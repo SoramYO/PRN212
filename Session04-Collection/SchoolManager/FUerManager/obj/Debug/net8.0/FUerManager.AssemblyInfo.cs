6e81160e4be9ad7155970b696a059eeaa8553379
Xuan Son <ngoxuanson121@gmail.com>
2024-06-15 14:55:28 +0700

    up15/6

--- Session04-Collection/SchoolManager/FUerManager/obj/Debug/net8.0/FUerManager.AssemblyInfo.cs
+++ Session04-Collection/SchoolManager/FUerManager/obj/Debug/net8.0/FUerManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUerManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4722c9f24dd78ab0f04a73ef843cf78a20470dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07cb0ebe89e081c2a0ed76cfa2f927972263e4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUerManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUerManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Session06-Database/BookStore/BookManager/BookStoreContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using BookManager.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManager;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext()
+    {
+    }
+
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local);Database= BookStore;UID=sa;PWD=12345;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__Book__3DE0C2075E939B59");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.BookId).ValueGeneratedNever();
+            entity.Property(e => e.Author).HasMaxLength(50);
+            entity.Property(e => e.BookName).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(1000);
+            entity.Property(e => e.PublicationDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.BookCategory).WithMany(p => p.Books)
+                .HasForeignKey(d => d.BookCategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Book_BookCategory");
+        });
+
+        modelBuilder.Entity<BookCategory>(entity =>
+        {
+            entity.HasKey(e => e.BookCategoryId).HasName("PK__BookCate__6347EC0477D25612");
+
+            entity.ToTable("BookCategory");
+
+            entity.Property(e => e.BookCategoryId).ValueGeneratedNever();
+            entity.Property(e => e.BookGenreType).HasMaxLength(50);
+            entity.Property(e => e.Description).HasMaxLength(500);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
